707d1a641d8de1499abb29f484a2cecd67310e16
abnerlucss <abner.santos@bandtec.com.br>
2021-08-04 14:36:11 -0300

    Commit inicial

---
+++ src/main/java/com/example/abnerlucss/msvticket/MsvTicketApplication.java
@@ -0,0 +1,13 @@
+package com.example.abnerlucss.msvticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvTicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/abnerlucss/msvticket/MsvTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.abnerlucss.msvticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
