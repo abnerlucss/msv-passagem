52e3709e77754278b6538ff57df63d44bc2d18e7
abnerlucss <abner.santos@bandtec.com.br>
2021-08-26 12:31:22 -0300

    Alterando url da requisição do MsvEmbarque

--- src/main/java/com/example/abnerlucss/passagem/service/MsvEmbarque.java
+++ src/main/java/com/example/abnerlucss/passagem/service/MsvEmbarque.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "embarque" , url = "http://localhost:8081/v1/embarque")
+@FeignClient(name = "embarque" , url = "https://msv-embarque.herokuapp.com/v1/embarque")
 public interface MsvEmbarque {
 
     @RequestMapping(method = RequestMethod.POST, value = "/")
